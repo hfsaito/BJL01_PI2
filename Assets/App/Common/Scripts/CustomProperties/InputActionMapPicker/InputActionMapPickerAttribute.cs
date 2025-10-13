21ce95fdd44f6837c26d3bc2eefaf2571eb5006a
hfsaito <hiroshi.fs94@gmail.com>
2025-10-13 02:07:10 -0300

    feat: camera control

---
+++ Assets/App/Common/Scripts/CustomProperties/InputActionMapPicker/InputActionMapPickerAttribute.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+namespace Assets.App.Common.Scripts.CustomProperties.InputActionMapPicker
+{
+    public class InputActionMapPickerAttribute : PropertyAttribute { }
+}
---
+++ Assets/App/Common/Scripts/CustomProperties/InputActionPicker/InputActionPickerAttribute.cs
@@ -0,0 +1,24 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Assets.App.Common.Scripts.CustomProperties.InputActionPicker
+{
+    public class InputActionPickerAttribute : PropertyAttribute
+    {
+        public InputActionType inputActionType;
+        public string controlType;
+
+        public InputActionPickerAttribute(InputActionType inputActionType)
+        {
+            this.inputActionType = inputActionType;
+            controlType = "";
+        }
+
+        public InputActionPickerAttribute(InputActionType inputActionType, Type controlType)
+        {
+            this.inputActionType = inputActionType;
+            this.controlType = controlType.Name;
+        }
+    }
+}
